b56e5ffab34d65ce6f978bdc014bc54a6c615e5a
Rohan Rajput <rohanrajput0362@gmail.com>
2025-10-18 10:16:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "well-trained-freak-bc3s8",
+  "appId": "1:28358839:web:9e0e7ba54a5aafbe0a3532",
+  "apiKey": "AIzaSyAuBiCFf0IyXkfBXkbXrclRwD5W79EqpzA",
+  "authDomain": "well-trained-freak-bc3s8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "28358839"
+};
