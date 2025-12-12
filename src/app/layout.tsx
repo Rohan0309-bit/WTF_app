00d2a4f5a5778606a6734c9550e702243377bb65
Rohan Rajput <rohanrajput0362@gmail.com>
2025-12-12 14:28:34 +0000

    Try fixing this error: `Runtime Error: Error: useFirebase must be used w

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Toaster } from "@/components/ui/toaster"
 import './globals.css';
 import NextNProgress from 'nextjs-progressbar';
 import { ThemeProvider } from '@/components/theme-provider';
+import { FirebaseClientProvider } from '@/firebase/client-provider';
 
 // This is a client component, so we can't use metadata export
 // export const metadata: Metadata = {
@@ -27,11 +28,13 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
-        <ThemeProvider attribute="class" defaultTheme="dark">
-            <NextNProgress color="#EF4444" height={3} />
-            {children}
-            <Toaster />
-        </ThemeProvider>
+        <FirebaseClientProvider>
+          <ThemeProvider attribute="class" defaultTheme="dark">
+              <NextNProgress color="#EF4444" height={3} />
+              {children}
+              <Toaster />
+          </ThemeProvider>
+        </FirebaseClientProvider>
       </body>
     </html>
   );
